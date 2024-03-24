24847cedc9b128214109d5920a28dca075ff292f
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2024-03-24 19:22:55 +0600

    first commit

--- index.js
+++ index.js
@@ -1,103 +1,188 @@
-const express = require('express');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+};
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.options("", cors(corsConfig));
+
 app.use(express.json());
 
 // MongoDB Connection URL
 const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-        // Connect to MongoDB
-        await client.connect();
-        console.log("Connected to MongoDB");
+  try {
+    // Connect to MongoDB
+    await client.connect();
+    console.log("Connected to MongoDB");
 
-        const db = client.db('assignment');
-        const collection = db.collection('users');
+    const db = client.db("assignment");
+    const userCollection = db.collection("users");
+    const suppliesCollection = db.collection("supplies");
 
-        // User Registration
-        app.post('/api/v1/register', async (req, res) => {
-            const { name, email, password } = req.body;
+    // User Registration
+    app.post("/api/v1/register", async (req, res) => {
+      const { name, email, password } = req.body;
 
-            // Check if email already exists
-            const existingUser = await collection.findOne({ email });
-            if (existingUser) {
-                return res.status(400).json({
-                    success: false,
-                    message: 'User already exists'
-                });
-            }
-
-            // Hash the password
-            const hashedPassword = await bcrypt.hash(password, 10);
-
-            // Insert user into the database
-            await collection.insertOne({ name, email, password: hashedPassword });
-
-            res.status(201).json({
-                success: true,
-                message: 'User registered successfully'
-            });
+      // Check if email already exists
+      const existingUser = await collection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exists",
         });
+      }
 
-        // User Login
-        app.post('/api/v1/login', async (req, res) => {
-            const { email, password } = req.body;
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-            // Find user by email
-            const user = await collection.findOne({ email });
-            if (!user) {
-                return res.status(401).json({ message: 'Invalid email or password' });
-            }
+      // Insert user into the database
+      await userCollection.insertOne({ name, email, password: hashedPassword });
 
-            // Compare hashed password
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-                return res.status(401).json({ message: 'Invalid email or password' });
-            }
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+      });
+    });
 
-            // Generate JWT token
-            const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: process.env.EXPIRES_IN });
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
 
-            res.json({
-                success: true,
-                message: 'Login successful',
-                token
-            });
-        });
+      // Find user by email
+      const user = await userCollection.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
 
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
 
-        // ==============================================================
-        // WRITE YOUR CODE HERE
-        // ==============================================================
+      // Generate JWT token
+      const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+        expiresIn: process.env.EXPIRES_IN,
+      });
 
+      res.json({
+        success: true,
+        message: "Login successful",
+        token,
+      });
+    });
 
-        // Start the server
-        app.listen(port, () => {
-            console.log(`Server is running on http://localhost:${port}`);
-        });
+    // ==============================================================
+    // get all supplies
+    app.get("/api/v1/supplies", async (req, res) => {
+      const result = await suppliesCollection.find().toArray();
 
-    } finally {
-    }
+      res.status(201).json({
+        success: true,
+        message: "Supplies retrieve successfully!",
+        data: result,
+      });
+    });
+
+    app.post("/api/v1/supply", async (req, res) => {
+      const data = req.body;
+      const result = await suppliesCollection.insertOne(data);
+
+      res.status(201).json({
+        success: true,
+        message: "Supplies created successfully!",
+        data: result,
+      });
+    });
+
+    // get supply details
+    app.get("/api/v1/supply/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await suppliesCollection.findOne(query);
+
+      res.status(201).json({
+        success: true,
+        message: "Supply is retrieve successfully!",
+        data: result,
+      });
+    });
+
+    // get supply details
+    app.put("/api/v1/supply/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedData = {
+        $set: {
+          img: data.img,
+          title: data.title,
+          category: data.category,
+          price: data.price,
+          description: data.description,
+        },
+      };
+      const result = await suppliesCollection.updateOne(query, updatedData);
+
+      res.status(201).json({
+        success: true,
+        message: "Supply is updated successfully!",
+        data: result,
+      });
+    });
+
+    app.delete("/api/v1/supply/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await suppliesCollection.deleteOne(query);
+
+      res.status(201).json({
+        success: true,
+        message: "Supply is deleted successfully!",
+        data: result,
+      });
+    });
+
+    // ==============================================================
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
 // Test route
-app.get('/', (req, res) => {
-    const serverStatus = {
-        message: 'Server is running smoothly',
-        timestamp: new Date()
-    };
-    res.json(serverStatus);
+app.get("/", (req, res) => {
+  const serverStatus = {
+    message: "Server is running smoothly",
+    timestamp: new Date(),
+  };
+  res.json(serverStatus);
 });
